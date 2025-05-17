3f0d1eda72032b5d6357a23ca8934c148f8886b2
whatbest121 <98254183+whatbest121@users.noreply.github.com>
2025-05-17 22:58:49 +0700

    create project nextJs Ui shadCn + tailwindcss

---
+++ my-project/src/app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className=" ">
+
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+
+      <Button>Click me</Button>
+    </div>
+  );
+}
